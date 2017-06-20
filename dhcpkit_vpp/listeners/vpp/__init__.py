fb84880510d1d0426ed7a7d52cd99a0900e23ae1
Sander Steffann <sander@steffann.nl>
2017-06-21 00:35:47 +0200

    Add more detailed exceptions

--- dhcpkit_vpp/listeners/vpp/__init__.py
+++ dhcpkit_vpp/listeners/vpp/__init__.py
@@ -1,3 +1,22 @@
 """
 Factory for the implementation of a listener on a VPP punt socket
 """
+from dhcpkit.ipv6.server.listeners import IgnoreMessage
+
+
+class UnwantedVPPMessage(IgnoreMessage):
+    """
+    This is a message that we don't want
+    """
+
+
+class UnknownVPPInterface(UnwantedVPPMessage):
+    """
+    Signal that this message is incomplete because it came from an unknown VPP interface.
+    """
+
+
+class UnknownVPPAction(UnwantedVPPMessage):
+    """
+    Signal that this message is incomplete because it contained an unknown VPP action value.
+    """
